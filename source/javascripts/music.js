18a04a0a37b41347507588e64c46b9b022e88b2a
Sadie Balsom <saralogsin@protonmail.com>
2019-11-20 14:37:41 +0100

    edits made to page seven

--- source/javascripts/music.js
+++ source/javascripts/music.js
@@ -1,11 +1,17 @@
+const invisibles = document.querySelectorAll('.invisible');
+
 const appearWords = () => {
-  console.log('here');
+  invisibles.velocity({
+  opacity:1
+}, {
+  stagger: 1000,
+  duration: 1500 });
 }
 
 const handleMusicScroll = () => {
+  console.log(body.getBoundingClientRect()["y"])
 
-  if(body.getBoundingClientRect()["y"] <= -4698) {
-    console.log(body.getBoundingClientRect()["y"])
+  if(body.getBoundingClientRect()["y"] <= -5200) {
 
   appearWords();
 
