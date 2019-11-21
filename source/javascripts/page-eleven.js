3022c9226ff9d124d3ea07c3b4f3d43e8c1c4dcd
Sadie Balsom <saralogsin@protonmail.com>
2019-11-21 10:47:41 +0100

    page eleven animation aligned with page scroll and sped up

--- source/javascripts/page-eleven.js
+++ source/javascripts/page-eleven.js
@@ -26,40 +26,40 @@ const cheeks = document.getElementById('cheeks');
 
 const swingCurtain = () => {
   // the last line is set on its own timer to appear last
-  stanzaThree.velocity({opacity: 1}, {duration: 1500, delay: 20000, stagger:1000})
+  stanzaThree.velocity({opacity: 1}, {duration: 1500, delay: 3000, stagger:1000})
 
 // each word comes in separately at random intervals
-  stroke.velocity({opacity: 1}, {duration: 1500, delay: 500})
-  erased.velocity({opacity: 1}, {duration: 1500, delay: 1500})
-  smudging.velocity({opacity: 1}, {duration: 1500, delay: 2500})
-  cheeks.velocity({opacity: 1}, {duration: 1500, delay: 3500})
-  mouthForced.velocity({opacity: 1}, {duration: 1500, delay: 4500})
-  through.velocity({opacity: 1}, {duration: 1500, delay: 500})
-  ruined.velocity({opacity: 1}, {duration: 1500, delay: 6500})
-  next.velocity({opacity: 1}, {duration: 1500, delay: 7500})
-  blurred.velocity({opacity: 1}, {duration: 1500, delay: 8500})
-  open.velocity({opacity: 1}, {duration: 1500, delay: 9500})
-  stretched.velocity({opacity: 1}, {duration: 1500, delay: 10500})
-  eyelashes.velocity({opacity: 1}, {duration: 1500, delay: 11500})
-  penciling.velocity({opacity: 1}, {duration: 1500, delay: 12500})
-  features.velocity({opacity: 1}, {duration: 1500, delay: 13500})
-  fabric.velocity({opacity: 1}, {duration: 1500, delay: 14500})
-  face.velocity({opacity: 1}, {duration: 1500, delay: 15500})
-  nose.velocity({opacity: 1}, {duration: 1500, delay: 16500})
+  stroke.velocity({opacity: 1}, {duration: 500})
+  erased.velocity({opacity: 1}, {duration: 500, delay: 500})
+  smudging.velocity({opacity: 1}, {duration: 500, delay: 600})
+  cheeks.velocity({opacity: 1}, {duration: 500, delay: 700})
+  mouthForced.velocity({opacity: 1}, {duration: 500, delay: 800})
+  through.velocity({opacity: 1}, {duration: 500, delay: 900})
+  ruined.velocity({opacity: 1}, {duration: 500, delay: 1000})
+  next.velocity({opacity: 1}, {duration: 500, delay: 1100})
+  blurred.velocity({opacity: 1}, {duration: 500, delay: 1200})
+  open.velocity({opacity: 1}, {duration: 500, delay: 1300})
+  stretched.velocity({opacity: 1}, {duration: 500, delay: 1400})
+  eyelashes.velocity({opacity: 1}, {duration: 500, delay: 1500})
+  penciling.velocity({opacity: 1}, {duration: 500, delay: 1600})
+  features.velocity({opacity: 1}, {duration: 500, delay: 1700})
+  fabric.velocity({opacity: 1}, {duration: 500, delay: 1800})
+  face.velocity({opacity: 1}, {duration: 500, delay: 1900})
+  nose.velocity({opacity: 1}, {duration: 500, delay: 2000})
 
 // as the words are appearing separately, the lines come and fill in one by one
-  lineOne.velocity({opacity: 1}, {duration: 1500, stagger: 100})
-  lineTwo.velocity({opacity: 1}, {duration: 1500, stagger: 100})
-  lineThree.velocity({opacity: 1}, {duration: 1500, stagger: 100})
-  lineFour.velocity({opacity: 1}, {duration: 1500, stagger: 100})
-  lineFive.velocity({opacity: 1}, {duration: 1500, stagger: 100})
-  lineSix.velocity({opacity: 1}, {duration: 1500, stagger: 100})
+  lineOne.velocity({opacity: 1}, {duration: 500, stagger: 100})
+  lineTwo.velocity({opacity: 1}, {duration: 500, stagger: 100})
+  lineThree.velocity({opacity: 1}, {duration: 500, stagger: 100})
+  lineFour.velocity({opacity: 1}, {duration: 500, stagger: 100})
+  lineFive.velocity({opacity: 1}, {duration: 500, stagger: 100})
+  lineSix.velocity({opacity: 1}, {duration: 500, stagger: 100})
 }
 
 // rename this function and use the scrollY instead of this rectangle thing
 // it used to have a curtain effect, I just kept the name of the animation the same
 const handleCurtainScroll = () => {
-  if(body.getBoundingClientRect()["y"] <= -8000) {
+  if(window.scrollY > 10683 ) {
 
   swingCurtain();
 
